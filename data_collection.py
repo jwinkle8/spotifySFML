936d2d3e1b08e718e01beee1226b3de7e9f7eae7
Your Name <jwinkle@jhu.edu>
2022-01-16 21:02:42 -0500

    Complete song retrieval for my own songs

--- data_collection.py
+++ data_collection.py
@@ -1,50 +1,48 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+import csv
 import sys
+import spotipy
+import spotipy.util as util
 
+def write_songs(sp):
 
-def show_tracks(tracks):
-    for i, item in enumerate(tracks['items']):
-        track = item['track']
-        print("   %d %32.32s %s" % (i, track['artists'][0]['name'],
-            track['name']))
+    with open('SFML/spotifySFML/songs.csv', 'w') as songs_file:
+        writer = csv.writer(songs_file)
+        for i in range(200):
+            results = sp.current_user_saved_tracks(50, i*50)
+            if not results['items']:
+                break
 
+            song_num = 1
+            for item in results['items']:
+                track = item['track']
+                track_audio = sp.audio_features(track['uri'])[0]
+                track_info = [str(i * 50 + song_num), 
+                            track['name'], 
+                            track['artists'][0]['name'],
+                            track_audio['danceability'],
+                            track_audio['energy'],
+                            track_audio['loudness'],
+                            track_audio['speechiness'],
+                            track_audio['acousticness'],
+                            track_audio['liveness'],
+                            track_audio['tempo']]
+                writer.writerow(track_info)
+                song_num += 1
+
+    songs_file.close()
 
 if __name__ == '__main__':
-
-    with open("SFML/spotifySFML/dev_secrets.txt") as dev_secrets:
-        cid = dev_secrets.readline().split(":")[1]
-        secret = dev_secrets.readline().split(":")[1]
-
-    #Authentication - without user
-    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-    sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+    scope = 'user-library-read'
 
     if len(sys.argv) > 1:
         username = sys.argv[1]
     else:
-        print("Whoops, need your username!")
-        print("usage: python user_playlists.py [username]")
+        print("Usage: %s username" % (sys.argv[0],))
         sys.exit()
 
-    scope = "user-library-read"
-    jwinkle8 = spotipy.util.prompt_for_user_token(username, scope=scope, client_id=cid, client_secret=secret, redirect_uri="https:/example.com/callback/")
+    token = util.prompt_for_user_token(username, scope)
 
-    if jwinkle8:
-        sp = spotipy.Spotify(auth=jwinkle8)
-        playlists = sp.user_playlists(username)
-        print(playlists)
-        for playlist in playlists['items']:
-            if playlist['owner']['id'] == username:
-                print()
-                print(playlist['name'])
-                print ('  total tracks', playlist['tracks']['total'])
-                results = sp.playlist(playlist['id'],
-                    fields="tracks,next")
-                tracks = results['tracks']
-                show_tracks(tracks)
-                while tracks['next']:
-                    tracks = sp.next(tracks)
-                    show_tracks(tracks)
+    if token:
+        write_songs(spotipy.Spotify(auth=token))
     else:
         print("Can't get token for", username)
